211aaa646e2f1b0ce6ba2c67dfba32409da3036b
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-04-07 23:04:01 -0400

    Agregadas funciones para pasar el parseado a numpy array y agregar un 0 de coordenada z

--- Tarea1a/Modulo/csvtolist.py
+++ Tarea1a/Modulo/csvtolist.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 #TODO agregar comentarios
 
@@ -20,16 +21,56 @@ def ubicacion_caidas(data):
 
 def borrado_x(a):
     if 'x' in a:
-        print(a)
+       # print(a)
         a=a.replace('x','')
-        print(a)
+        #print(a)
     return int(a)
 
 def parseado_coordenada(parse):
     l = []
     for lista in parse:
         l.append(list(map(borrado_x, lista)))   
-    print(parse)
+   # print(parse)
     return l 
 
+def agregar_coord_z(lista):
+    for curva in lista:
+        for coordenada in curva:
+            coordenada.append(0)
+    return lista
 
+###Primero parseado, luego parseado_coordenada
+
+def list_to_numpy(lista):
+    return np.array(lista)    
+
+
+def crater(lista,lista_x):
+    salida=[]
+    actual=[]
+    crateres=0
+    for t in lista:
+        for x in lista_x:
+            if t[0]==x[0] and t[1]==x[1]:
+                actual.append([t[0]-0.3,t[1]])
+                actual.append([t[0]+0.3,t[1]])
+
+                salida.append(actual)
+                actual=[[t[0]-0.3,t[1]],[t[0]+0.3,t[1]]]
+            else:
+                actual.append([t[0],t[1]])
+    if not (actual==[]):
+        salida.append(actual)
+    return salida                
+
+
+a=parsear("datos.csv")
+
+b=parseado_coordenada(a)
+caidas=ubicacion_caidas(a)
+
+hoyos=crater(b,caidas)
+print(hoyos)
+c=agregar_coord_z(hoyos)
+
+print(c)
