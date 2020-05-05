b7e4971373a1804d6e0a23b3cc5367f7bb623634
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-05-05 16:37:25 -0400

    Avance en la función que dibuja al pajaro

--- Tarea2c/bird.py
+++ Tarea2c/bird.py
@@ -7,7 +7,9 @@ import sys
 import Modulo.transformations as tr
 import Modulo.basic_shapes as bs
 import Modulo.scene_graph as sg
-#import easy_shaders as es
+import Modulo.easy_shaders as es
+import Modulo.readobj as rbj
+import Modulo.lighting_shaders as ls
 #import lighting_shaders as ls
 
 
@@ -19,15 +21,73 @@ def on_key(window, key, scancode, action, mods):
         sys.exit()
 
 
+
+
+
+def pajaro():
+    gpuCabeza = es.toGPUShape(rbj.readOBJ("Model/cabeza.obj", (0,1,1)))
+    gpuTorso = es.toGPUShape(rbj.readOBJ("Model/torso.obj", (0,1,1)))
+    gpuAlaSupDer = es.toGPUShape(rbj.readOBJ("Model/alasupder.obj", (0,1,1)))
+    gpuAlaInfDer = es.toGPUShape(rbj.readOBJ("Model/cabeza.obj", (0,1,1)))
+    gpuAlaSupIzq = es.toGPUShape(bs.createColorCube(1,1,1))
+    gpuAlaInfIzq = es.toGPUShape(bs.createColorCube(1,1,1))
+
+
+    #Ala inf derecha
+    AlaInfDerecha = sg.SceneGraphNode("AlaInfDerecha")
+    AlaInfDerecha.transform = tr.translate(0,0,0)  #Ajustar bn
+    AlaInfDerecha.childs+=[gpuAlaInfDer]
+
+    #Rotación ala inf derecha
+    RotacionAlaInfDerecha = sg.SceneGraphNode("RotacionAlaInfDerecha")
+    RotacionAlaInfDerecha.transform = tr.translate(0,0,0)  #Ajustar bn
+    RotacionAlaInfDerecha.childs+= [AlaInfDerecha]
+
+    #Ala sup derecha
+    AlaSupDerecha = sg.SceneGraphNode("AlaSupDerecha")
+    AlaSupDerecha.childs+=[gpuAlaSupDer]
+
+    #Ala derecha
+    AlaDerecha = sg.SceneGraphNode("AlaDerecha")
+    AlaDerecha.childs+=[AlaSupDerecha]
+    AlaDerecha.childs+=[RotacionAlaInfDerecha]
+
+    #Alas
+    Alas = sg.SceneGraphNode("Alas")
+    Alas.childs+=[AlaDerecha]
+    #Alas.childs+=[AlaIzquierda]
+
+    #Torso
+    Torso = sg.SceneGraphNode("Torso")
+    Torso.childs+= [gpuTorso]
+
+    #Cabeza
+    Cabeza = sg.SceneGraphNode("Cabeza")
+    Cabeza.childs+= [gpuCabeza]
+    
+    #Cuerpo
+    Cuerpo = sg.SceneGraphNode("Cuerpo")
+    Cuerpo.childs+= [Torso]
+    Cuerpo.childs+= [Cabeza]
+
+    #Pajaro
+    Pajaro = sg.SceneGraphNode("Pajaro")
+    Pajaro.childs+=[Alas]
+
+    return Pajaro
+
+
+
 if __name__ == "__main__":
 
+    # Initialize glfw
     if not glfw.init():
         sys.exit()
 
-    width = 700
-    height = 700
+    width = 600
+    height = 600
 
-    window = glfw.create_window(width, height, "A", None, None)
+    window = glfw.create_window(width, height, "Reading a *.obj file", None, None)
 
     if not window:
         glfw.terminate()
@@ -38,37 +98,113 @@ if __name__ == "__main__":
     # Connecting the callback function 'on_key' to handle keyboard events
     glfw.set_key_callback(window, on_key)
 
-    # Assembling the shader program (pipeline) with both shaders
-    #pipeline = es.SimpleTransformShaderProgram()
+    # Defining shader programs
+    #pipeline = ls.SimpleFlatShaderProgram()
+    pipeline = ls.SimpleGouraudShaderProgram()
+    #pipeline = ls.SimplePhongShaderProgram()
+    mvpPipeline = es.SimpleModelViewProjectionShaderProgram()
 
-    
     # Telling OpenGL to use our shader program
-    #glUseProgram(pipeline.shaderProgram)
+    glUseProgram(pipeline.shaderProgram)
 
     # Setting up the clear screen color
     glClearColor(0.85, 0.85, 0.85, 1.0)
 
-    # Our shapes here are always fully painted
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+    # As we work in 3D, we need to check which part is in front,
+    # and which one is at the back
+    glEnable(GL_DEPTH_TEST)
 
-     #Enable transparency
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    # Creating shapes on GPU memory
+    gpuAxis = es.toGPUShape(bs.createAxis(7))
+    #gpuSuzanne = es.toGPUShape(shape = readOBJ('suzanne.obj', (0.9,0.6,0.2)))
+    gpuSuzanne = es.toGPUShape(shape = rbj.readOBJ('Model/alasupder.obj', (0.9,0.6,0.2)))
+    pajarito=pajaro()
+    #gpuCarrot = es.toGPUShape(shape = readOBJ('carrot.obj', (0.6,0.9,0.5)))
 
-
+    
+    t0 = glfw.get_time()
+    camera_theta = -3*np.pi/4
 
     while not glfw.window_should_close(window):
         # Using GLFW to check for input events
         glfw.poll_events()
 
+        # Getting the time difference from the previous iteration
+        t1 = glfw.get_time()
+        dt = t1 - t0
+        t0 = t1
+
+        if (glfw.get_key(window, glfw.KEY_LEFT) == glfw.PRESS):
+            camera_theta -= 2 * dt
+
+        if (glfw.get_key(window, glfw.KEY_RIGHT) == glfw.PRESS):
+            camera_theta += 2* dt
+
+        # Setting up the view transform
+        R = 12
+        camX = R * np.sin(camera_theta)
+        camY = R * np.cos(camera_theta)
+        viewPos = np.array([camX, camY, 7])
+        view = tr.lookAt(
+            viewPos,
+            np.array([0,0,1]),
+            np.array([0,0,1])
+        )
+
+        # Setting up the projection transform
+        projection = tr.perspective(60, float(width)/float(height), 0.1, 100)
+
         # Clearing the screen in both, color and depth
-        glClear(GL_COLOR_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Filling or not the shapes depending on the controller state
+        #if (controller.fillPolygon):
+        #    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        #else:
+        #    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        # Drawing shapes
+        glUseProgram(pipeline.shaderProgram)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
+
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ka"), 0.2, 0.2, 0.2)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Kd"), 0.9, 0.9, 0.9)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ks"), 1.0, 1.0, 1.0)
+
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "lightPosition"), -3, 0, 3)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "viewPosition"), viewPos[0], viewPos[1], viewPos[2])
+        glUniform1ui(glGetUniformLocation(pipeline.shaderProgram, "shininess"), 100)
+        glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "constantAttenuation"), 0.001)
+        glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "linearAttenuation"), 0.1)
+        glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "quadraticAttenuation"), 0.01)
+
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE, tr.uniformScale(3))
 
 
+        ###
+        sg.drawSceneGraphNode(pajarito, pipeline, "model")
+        #pipeline.drawShape(gpuSuzanne)
 
+            ##
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE,
+            tr.matmul([
+                tr.uniformScale(3),
+                tr.rotationX(np.pi/2),
+                tr.translate(1.5,-0.25,0)])
+        )
+        #pipeline.drawShape(gpuCarrot)
+        
+        glUseProgram(mvpPipeline.shaderProgram)
+        glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+        glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
+        mvpPipeline.drawShape(gpuAxis, GL_LINES)
 
-        # Once the render is done, buffers are swapped, showing only the complete scene.
+        # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
         glfw.swap_buffers(window)
 
-    
     glfw.terminate()
