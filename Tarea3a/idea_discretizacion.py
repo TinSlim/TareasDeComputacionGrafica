77ccb78fb83997acc3995f9db67666ec0254a3eb
TinSlim <cristobal.torres.g@ug.uchile.cl>
2020-06-18 00:24:03 -0400

    Avance en condiciones de borde

--- Tarea3a/idea_discretizacion.py
+++ Tarea3a/idea_discretizacion.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 W = 4  #X
@@ -19,3 +19,124 @@ def getIJK(k):
     y = (k%(nh*nw))//nw
     x = (k%(nh*nw))%nw
     return (x,y,z)
+
+
+for z in range(nh):
+    for y in range(nl):
+        for x in range(nw):
+            k = getK(x,y,z)
+            
+            k_x = getK(x+1,y,z)
+            k_-x = getK(x-1,y,z)
+            k_y = getK(x,y+1,z)
+            k_-y = getK(x,y-1,z)
+            k_z = getK(x,y,z+1)
+            k_-z = getK(x,y,z-1)
+            
+            #centro
+            if (1<=x and x<=nw -2) and (1<=y and y<=nl-2) and (1<=z and z<=nh-2):
+                A[k, k_x] = 1
+                A[k, k_-x] = 1
+                A[k, k_y] = 1
+                A[k, k_-y] = 1
+                A[k, k_z] = 1
+                A[k, k_-z] = 1
+                A[k,k] = -4
+                b[k] = 0
+
+            ##########################
+            #+x
+            elif (x==nw-1) and (1<=y and y<=nl-2) and (1<=z and z<=nh-2):
+                A[k, k_x] = 0
+                A[k, k_-x] = 1 
+                A[k, k_y] = 1
+                A[k, k_-y] = 1
+                A[k, k_z] = 1
+                A[k, k_-z] = 1
+                A[k,k] = -4
+                b[k] = 0
+
+            #-x
+            elif (x=1) and (1<=y and y<=nl-2) and (1<=z and z<=nh-2):
+                A[k, k_x] =1
+                A[k, k_-x] = 0
+                A[k, k_y] = 1
+                A[k, k_-y] = 1
+                A[k, k_z] = 1
+                A[k, k_-z] = 1
+                A[k,k] = -4
+                b[k] = 0
+
+            #+y
+            elif (1<=x and x<=nw -2) and (y==nl-1) and (1<=z and z<=nh-2):
+                A[k, k_x] = 1
+                A[k, k_-x] = 1 
+                A[k, k_y] = 0
+                A[k, k_-y] = 1
+                A[k, k_z] = 1
+                A[k, k_-z] = 1
+                A[k,k] = -4
+                b[k] = 0
+
+            #-y
+            elif (1<=x and x<=nw -2) and (y==1) and (1<=z and z<=nh-2):
+                A[k, k_x] = 1
+                A[k, k_-x] = 1 
+                A[k, k_y] = 1
+                A[k, k_-y] = 0
+                A[k, k_z] = 1
+                A[k, k_-z] = 1
+                A[k,k] = -4
+                b[k] = 0
+
+            #+z
+            elif (1<=x and x<=nw -2) and (1<=y and y<=nl-2) and (z==nh-1):
+                A[k, k_x] = 1
+                A[k, k_-x] = 1 
+                A[k, k_y] = 1
+                A[k, k_-y] = 1
+                A[k, k_z] = 0
+                A[k, k_-z] = 1
+                A[k,k] = -4
+                b[k] = 0
+
+            #-z Agregar heater
+            elif (1<=x and x<=nw -2) and (1<=y and y<=nl-2) and (z==1):
+                A[k, k_x] = 1
+                A[k, k_-x] = 1 
+                A[k, k_y] = 1
+                A[k, k_-y] = 1
+                A[k, k_z] = 1
+                A[k, k_-z] = 0
+                A[k,k] = -4
+                b[k] = 0
+
+            #############################
+            elif (1<=x and x<=nw -2) and (y==nl-1) and (z==nh-1):
+            
+            elif (1<=x and x<=nw -2) and (y==1) and (z==nh-1):
+
+            elif (x==nw -1) and (1<=y and y<=nl-2) and (z==nh-1):
+            
+            elif (x==1) and (1<=y and y<=nl-2) and (z==nh-1):
+            
+            ###########################
+
+            elif (1<=x and x<=nw -2) and (y==nl-1) and (z==1):
+            
+            elif (1<=x and x<=nw -2) and (y==1) and (z==1):
+
+            elif (x==nw -1) and (1<=y and y<=nl-2) and (z==1):
+            
+            elif (x==1) and (1<=y and y<=nl-2) and (z==1):
+
+            
+            elif (x==nw -1) and (y==nl-1) and (1<=z and z<=nh-2):
+
+            elif (x==nw -1) and (y==1) and (1<=z and z<=nh-2):
+
+            elif (x==1) and (y==nl-1) and (1<=z and z<=nh-2):
+
+            elif (x==1) and (y==1) and (1<=z and z<=nh-2):
+            
+            
